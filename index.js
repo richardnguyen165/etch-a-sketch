e3ba07a75f3ee7e1c46cbb578d48e6ab2c2b87d7
richard.nguyen1 <richard.nguyen1@ucalgary.ca>
2025-06-19 22:27:25 -0600

    Finished the colours section, added a modal that allows user to input rgb, hex, or slider. Then allowed user to revert back to a default color, clear grid, and randomize color for each block.

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
 let dimension;
 let tracerResponseAnswer;
+let gridGenerated;
+let blockColor = '#000000';
+let randomClicked = false;
 
 const inputReference = document.querySelector('.dimension-input');
 const errorRefrence = document.querySelector('.info-area');
 const enterButton = document.querySelector('.enter-button');
+const colorButton = document.querySelector('.choose-a-color');
+const resetButton = document.querySelector('.reset-color');
+const resetGridButton = document.querySelector('.reset-grid');
+const randomButton =  document.querySelector('.random-color');
 
 enterButton.addEventListener('click', dimensionInput);
+colorButton.addEventListener('click', colorModal);
+resetButton.addEventListener('click', revertBack);
+resetGridButton.addEventListener('click', resetGrid);
+randomButton.addEventListener('click', randomColors);
+
+function randomColors(){
+  let allRows = document.querySelectorAll('.etch-a-sketch-block');
+  if (allRows.length === 0){
+    alert('No grid available. Please create grid.');
+    return;
+  }
+  if (!(randomClicked)){
+    allRows.forEach(item => {
+      item.addEventListener('mouseover', () => {
+        let randomNumber = Math.floor(Math.random() * 16777215);
+        item.style['background-color'] = `#${randomNumber.toString(16).padStart(6, '0')}`;
+      });
+    })
+  }
+  else{
+    allRows.forEach(item => {
+      item.addEventListener('mouseover', () => {
+        item.style['background-color'] = blockColor;
+      });
+    });
+  }
+  randomButton.classList.toggle('random-color-active');
+  randomClicked = !(randomClicked);
+}
+
+function resetGrid(){
+  let allRows = document.querySelectorAll('.etch-a-sketch-block');
+  if (allRows.length > 0){
+    allRows.forEach(item => {
+      item.style['background-color'] = `#ffffff`;
+    }
+  )
+  }
+  else{
+    alert('No grid available. Pleasae create grid.');
+  }
+}
+
+function revertBack(){
+  if (randomClicked){
+    randomButton.classList.toggle('random-color-active');
+    randomClicked = !(randomClicked);
+  }
+  blockColor = '#000000';
+  let allRows = document.querySelectorAll('.etch-a-sketch-block');
+  if (allRows.length > 0){
+    allRows.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.style['background-color'] = blockColor;
+      });
+    })
+  }
+  else{
+    alert('No grid available. Pleasae create grid.');
+  }
+}
 
 function errorMessage(message){
   const paraErrorReference = document.createElement('p');
@@ -33,7 +101,6 @@ function dimensionInput(){
   else{
     dimension = dimensionTentative;
     tracerResponse();
-    // gridCreation()
   }
 }
 
@@ -62,7 +129,7 @@ function gridCreation(){
   // adding to html
   const outerContainerReference = document.querySelector('.etch-a-sketch-outer-container');
   outerContainerReference.innerHTML = rowsHTML;
-  outerContainerReference.style['width'] = `700px`
+  outerContainerReference.style['width'] = `900px`
 
   const outerGridReference = document.querySelectorAll('.etch-a-sketch-inner-container');
   outerGridReference.forEach(item => {
@@ -74,8 +141,8 @@ function gridCreation(){
   blockReference.forEach(item => {
     // so that the outercontainer actually contains the the rows (before, due to the border, it would overflow)
     item.style['box-sizing'] = 'border-box';
-    item.style['width'] = `${700 / dimension}px`;
-    item.style['height'] = `${700 / dimension}px`;;
+    item.style['width'] = `${900 / dimension}px`;
+    item.style['height'] = `${900 / dimension}px`;;
     item.style['border'] = 'solid';
     item.style['border-color'] = 'lightgrey';
     item.style['border-width'] = '1px';
@@ -84,7 +151,7 @@ function gridCreation(){
     let timer;
 
     item.addEventListener('mouseover', () => {
-      item.style['background-color'] = 'black';
+      item.style['background-color'] = blockColor;
     });
 
     if (tracerResponseAnswer){
@@ -100,8 +167,9 @@ function gridCreation(){
       });
     }
   });
-}
 
+  gridGenerated = true;
+}
 
 function tracerResponse(){
   // Used chatGPT (i had a similar answer -> but once again, see comment below which took me 2-5 hours to fix)
@@ -136,80 +204,180 @@ function tracerResponse(){
     gridCreation();
   });
 
-  const modalCancelRef = document.querySelector('.modal-cancel');
-  modalCancelRef.addEventListener('click', () => {
-  modalBackgroundRef.remove()
-})};
+    const modalCancelRef = document.querySelector('.modal-cancel');
+    modalCancelRef.addEventListener('click', () => {
+    modalBackgroundRef.remove()
+  })
+}
 
-/* 
-DO NOT DO:
-function tracerResponse(){
-  response = 
-  
-  <div class = "modal-background">
+function colorModal(){
+  if (gridGenerated){
+    let backgroundColor;
+     // https://www.freecodecamp.org/news/create-color-picker-using-html-css-and-javascript/
+    const colorModal = document.createElement('div');
+    colorModal.className = 'modal-background';
+    colorModal.innerHTML = 
+    `
     <div class = "modal-content">
-      <p class = "modal-content-header">Disappearance Option</p>
-      <p>After some set amount of time, would you like your path to disappear?</p>
+      <p class = "modal-content-header">Color Options</p>
+      <div class = "modal-color-options">
+        <div>
+          <ul>
+            <li>
+              All hex values must be six characters long
+            </li>
+            <li>
+              Inputting the buttons on the right does not cause the save to be automatic.
+              <br> Only pressing done does.
+            </li>
+            <li>
+              To discard changes, press the Cancel button.
+            </li>
+          </ul>
+        </div>
+        <div class = "modal-input-container">
+          <input class = "slider-color-input" type = "color" value="${blockColor}">
+          <button class = "modal-color-slider">Enter Slider</button>
+        </div>
+        <div class = "modal-input-container">
+          <p>Hex Color: #<input value="${blockColor.slice(1)}" class = "color-input-hex"></p>
+          <button class = "modal-color-hex">Enter Hex</button>
+        </div>
+        <div class = "modal-input-container">
+          <p>RGB Color: rgb( <input class = "color-rgb-first-part" value = "${parseInt(blockColor.slice(1, 3), 16)}">, <input class = "color-rgb-second-part" value = "${parseInt(blockColor.slice(3, 5), 16)}">, <input class = "color-rgb-third-part" value = "${parseInt(blockColor.slice(5), 16)}"> )</p>
+          <button class = "modal-color-rgb">Enter RGB</button>
+        </div>
+      </div>
+      <p class = "modal-valid-colour-warning"></p>
       <div>
-        <button class = "modal-yes">Yes</button>
-        <button class = "modal-no">No</button>
-        <button class = "modal-cancel">Cancel</button>
+        <button class = "modal-color-done">Done</button>
+        <button class = "modal-color-cancel">Cancel</button>
       </div>
     </div>
-  </div>
-  
-  ;
+    `;
 
-  document.body.innerHTML += response;
-THIS WILL WIPE OUT ALL OF THE EVENT LISTENERS IN THE CODE. (INCLUDING PREVIOUS ONES)
+    document.body.appendChild(colorModal);
+    const colorInputSelectorRef = document.querySelector('.slider-color-input');
 
-INSTEAD DO .appendChild()
+    const hexInputRef = document.querySelector('.color-input-hex');
 
-Why not? (per ChatGPT)
+    const firstRGBRef = document.querySelector('.color-rgb-first-part');
+    const secondRGBRef = document.querySelector('.color-rgb-second-part');
+    const thirdRGBRef = document.querySelector('.color-rgb-third-part');
 
-1. Reparsing and Replacing All Child Nodes
-When you do:
+    const modalCancelColorRef = document.querySelector('.modal-color-cancel');
+    modalCancelColorRef.addEventListener('click', () => colorModal.remove());
 
-element.innerHTML += newContent;
+    const rgbInputRef = document.querySelector('.modal-color-rgb');
+    rgbInputRef.addEventListener('click', () => {
+      backgroundColor = RGBtoHex(firstRGBRef, secondRGBRef, thirdRGBRef, colorInputSelectorRef, hexInputRef);
+    });
 
-It’s basically:
+    const modalColorHexRef = document.querySelector('.modal-color-hex');
+    modalColorHexRef.addEventListener('click', () => {
+      backgroundColor = hexToRGB(hexInputRef, colorInputSelectorRef, firstRGBRef, secondRGBRef, thirdRGBRef);
+    });
 
-Reading the current HTML string inside element
+    const modalColorSliderRef = document.querySelector('.modal-color-slider');
+    modalColorSliderRef.addEventListener('click', () => {
+      backgroundColor = sliderInput(colorInputSelectorRef.value, hexInputRef, firstRGBRef, secondRGBRef, thirdRGBRef);
+    });
 
-Appending your new HTML string to it
+    const doneRef = document.querySelector('.modal-color-done');
+    doneRef.addEventListener('click', () => {
+      if (backgroundColor){
+        changeHoverColor(backgroundColor);
+      }
+      else{
+        alert('Color not selected: Please select a color.');
+      }
+    });
 
-Replacing the entire inner content with the new combined string
+  }
+  else{
+    alert('Grid has not been generated.');
+  }
+}
 
-This means all existing DOM nodes inside that element get destroyed and recreated from scratch, losing all references and event listeners attached to them.
+function RGBtoHex(firstRGBRef, secondRGBRef, thirdRGBRef, colorInputSelectorRef, hexInputRef){
+  let firstNumber = Number(firstRGBRef.value);
+  let secondNumber = Number(secondRGBRef.value);
+  let thirdNumber = Number(thirdRGBRef.value);
+  console.log(firstNumber, secondNumber, thirdNumber);
+  if ((firstNumber === NaN || secondNumber === NaN) || thirdNumber === NaN){
+    alert('Invalid RGB: One of your numbers is non-numeric.');
+    return;
+  }
+  if (((firstNumber < 0 || firstNumber > 255) || (secondNumber < 0 || secondNumber > 255)) || (thirdNumber < 0 || thirdNumber > 255)){
+    alert('Invalid RGB: One of your numbers is out of range. Must be between 0-255.');
+    return;
+  }
+  let firstNumberHex = firstNumber.toString(16).padStart(2, '0');
+  let secondNumberHex = secondNumber.toString(16).padStart(2, '0');
+  let thirdNumberHex = thirdNumber.toString(16).padStart(2, '0');
+  let hexNumber = `#${firstNumberHex}${secondNumberHex}${thirdNumberHex}`;
+  colorInputSelectorRef.value = hexNumber;
+  hexInputRef.value = hexNumber.slice(1);
+  return hexNumber;
+}
 
-2. Event Listeners Are Lost
-Event listeners attached with addEventListener are stored in memory linked to the specific DOM nodes.
+function hexToRGB(hexStringer, colorInputSelectorRef, firstRGBRef, secondRGBRef, thirdRGBRef){
+  hexValue = hexStringer.value;
+  if (hexValue.length !== 6){
+    alert('Invalid hex: Must be 6 characters long (application only supports 6 characters long');
+    return;
+  }
+  const numberTest = parseInt("0x" + hexValue);
+  console.log(numberTest);
+  if (numberTest === NaN){
+    alert('Invalid hex: Please find a valid hex.');
+    return;
+  }
+  if (numberTest < 0 || numberTest > 16777215){
+    alert('Invalid hex: Please find a valid hex.');
+    return;
+  }
+  colorInputSelectorRef.value = `#${hexValue}`;
+  firstRGBRef.value = parseInt(hexValue.slice(0, 2), 16);
+  secondRGBRef.value = parseInt(hexValue.slice(2, 4), 16);
+  thirdRGBRef.value = parseInt(hexValue.slice(4), 16);
+  return `#${hexValue}`;
+}
 
-Destroy those nodes (by replacing innerHTML), and the listeners no longer exist.
+function sliderInput(colorInputSelectorRef, hexInputRef, firstRGBRef, secondRGBRef, thirdRGBRef){
+  let hexStringer = colorInputSelectorRef.slice(1);
+  if (hexStringer.length !== 6){
+    alert('Invalid hex: Must be 6 characters long (application only supports 6 characters long');
+    return;
+  }
+  const numberTest = parseInt(hexStringer, 16);
+  if (numberTest === NaN){
+    alert('Invalid hex: Please find a valid hex.');
+    return;
+  }
+  if (numberTest < 0 || numberTest > 16777215){
+    alert('Invalid hex: Please find a valid hex.');
+    return;
+  }
+  hexInputRef.value = colorInputSelectorRef.slice(1);
+  firstRGBRef.value = parseInt(hexStringer.slice(0, 2), 16);
+  secondRGBRef.value = parseInt(hexStringer.slice(2, 4), 16);
+  thirdRGBRef.value = parseInt(hexStringer.slice(4), 16);
+  return colorInputSelectorRef;
+}
 
-This can break user interaction unexpectedly and silently.
-
-3. Potential Performance Issues
-Repeatedly rewriting large chunks of HTML:
-
-Forces the browser to re-parse and re-render all the children of that element.
-
-This can be costly if the element is large or contains complex structures.
-
-Using DOM methods (appendChild, insertAdjacentHTML, etc.) lets the browser add only what’s needed without wiping everything.
-
-4. State and Focus Can Be Lost
-If users had focus inside an input or any interactive element inside that container, rewriting innerHTML will remove focus.
-
-Any JavaScript state you kept attached to DOM elements (like custom properties or data) will be lost.
-
-5. Unexpected Side Effects With Complex DOM
-If you have other scripts or third-party code relying on the existing DOM elements, wiping and recreating them can break assumptions and cause bugs.
-
-For example, if a plugin initialized on those elements, you’d have to re-initialize after the replace.
-
-6. Difficult to Maintain
-Using innerHTML += is a quick shortcut but makes code harder to reason about.
-
-Explicitly creating nodes and appending them is clearer, easier to debug, and safer long term.
-*/
+function changeHoverColor(hexColor){
+  if (randomClicked){
+    randomButton.classList.toggle('random-color-active');
+    randomClicked = !(randomClicked);
+  }
+  blockColor = hexColor;
+  const allBlocks = document.querySelectorAll('.etch-a-sketch-block');
+  allBlocks.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.style['background-color'] = blockColor;
+    });
+  });
+  const colorModal = document.querySelector('.modal-background');
+  colorModal.remove();
+}
